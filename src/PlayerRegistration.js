7f1546e54283d3afdc474d9b5be0d3abf58eb526
bulbenko2 <dsnbel@gmail.com>
2025-11-14 13:25:27 +0300

    PlayerReg ready

---
+++ src/PlayerRegistration.js
@@ -0,0 +1,32 @@
+
+const inquirer = require('inquirer').default
+const Player = require('./Player');
+
+class PlayerRegistration {
+  async createPlayer() {
+    const { name } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Введите ваше имя:',
+        validate: (value) => this.validateName(value),
+      },
+    ]);
+
+    return new Player(name.trim());
+  }
+
+  validateName(name) {
+    if (!name || name.trim().length < 3) {
+      return 'Имя должно быть длиной не менее 3 символов';
+    }
+    return true;
+  }
+
+  startSession(player, game) {
+    game.player = player;
+    game.play();
+  }
+}
+
+module.exports = PlayerRegistration;
